a45b547ff82b6cf73d377db5b6933fe12613b03a
Viacheslav ALEKSIEIEV <bagetta007@gmail.com>
2023-02-18 18:03:11 +0200

    update JS

--- src/js/category-search.js
+++ src/js/category-search.js
@@ -1,11 +1,15 @@
 import axios from 'axios';
 
-const API_KEY_NYT = '85VwA43v2JxogoVfbQi2IxsUQyUy8fL7';
+const API_KEY_NYT = 'uWCgMUrBBTLVIAv2yDHwKfGZbI4b2q7L';
+
+
+const categoriesDropdownList = document.querySelector('.categories-dropdown-list-js')
+const othersDropdownList = document.querySelector('.others-dropdown-list-js')
 
 async function getCategoryList() {
     const response = await axios(`https://api.nytimes.com/svc/news/v3/content/section-list.json?api-key=${API_KEY_NYT}`);
-    
-     const markupOthers = response.data.results.map((arr, index) => {
+   
+   const markupOthers = response.data.results.map((arr, index) => {
          if (index > 5) {
            return `<li class="dropdown__item">
             <button class="dropdown__category-btn">${arr.display_name}</button>
@@ -14,6 +18,10 @@ async function getCategoryList() {
     }).join('')
     othersDropdownList.innerHTML = markupOthers;
 
-  
+    const markupCategories = response.data.results.map(arr => `<li class="dropdown__item">
+                            <button class="dropdown__category-btn">${arr.display_name}</button>
+                        </li>`).join('');
+    categoriesDropdownList.innerHTML = markupCategories;
 }
+getCategoryList();
 
