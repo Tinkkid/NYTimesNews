8f115085c093dc357f6316307bc7b360a19867f6
MaxF1996 <maxeffect.apollo@gmail.com>
2023-02-20 13:37:19 +0200

    popular cards js

--- src/js/cardMarkup.js
+++ src/js/cardMarkup.js
@@ -1,7 +1,22 @@
-import { cutText, cutTitle, formatDate } from "./makkupUtils";
+import { cutText, cutTitle, formatDate } from './makkupUtils';
 
-export function createCard({section_name, web_url, headline, lead_paragraph, pub_date, multimedia, snippet, subsection, published_date, title, url, media, des_facet, abstract })
- {      return `
+export function createCard({
+  section_name,
+  web_url,
+  headline,
+  lead_paragraph,
+  pub_date,
+  multimedia,
+  snippet,
+  subsection,
+  published_date,
+  title,
+  url,
+  media,
+  des_facet,
+  abstract,
+}) {
+  return `
         <li class="news-item">
         <div class="overlay"></div>
         <div class="img-thumb">
@@ -10,7 +25,9 @@ export function createCard({section_name, web_url, headline, lead_paragraph, pub
         <svg class="icon-done"><use class="icon-arrow-done"
         href="./images/sprite.svg#arrow-down"></use></svg>
           </span>
-          <img src="https://www.nytimes.com/${multimedia[0].url || ''}" loading="lazy" alt="${snippet || des_facet}" class="news-img" />
+          <img src="https://www.nytimes.com/${multimedia[0].url || ''}" loading="lazy" alt="${
+    snippet || des_facet
+  }" class="news-img" />
           <p class="news-chip">${section_name || subsection}</p>
           <button type="button" class="add-news-favorite">
             <p class="favorite-btn-text">Add to favorite</p>
@@ -38,6 +55,57 @@ export function createCard({section_name, web_url, headline, lead_paragraph, pub
             >
           </div>
         </div>
-      </li> `
-} 
+      </li> `;
+}
 
+export function createCardPop({
+  media,
+  title,
+  section,
+  subsection,
+  abstract,
+  published_date,
+  url,
+}) {
+  console.log(media[0]['media-metadata'][['media-metadata'].length - 1].url);
+  return `
+        <li class="news-item">
+        <div class="overlay"></div>
+        <div class="img-thumb">
+        <span class="readable"
+        >Already read
+        <svg class="icon-done"><use class="icon-arrow-done"
+        href="./images/sprite.svg#arrow-down"></use></svg>
+          </span>
+          <img src="${media[0]['media-metadata'][2].url || ''}" loading="lazy" alt="${
+    media.caption
+  }" class="news-img" />
+          <p class="news-chip">${section || subsection}</p>
+          <button type="button" class="add-news-favorite">
+            <p class="favorite-btn-text">Add to favorite</p>
+            <svg class="favorite-icon" width="16" height="16">
+              <use href="./images/sprite.svg#heart-fill" class="icon-heart"></use>
+              <use href="./images/sprite.svg#heart-empty class="icon-empty-heart"></use>
+            </svg>
+          </button>
+        </div>
+        <div class="news-info">
+          <h2 class="news-title">
+            ${cutTitle(title)}
+          </h2>
+          <p class="news-desk">
+          ${cutText(abstract)}
+          </p>
+          <div class="adding">
+            <p class="news-date">${formatDate(published_date)}</p>
+            <a
+              href=${url}
+              class="news-link"
+              target="_blank"
+              rel="noopener noreferrer"
+              >Read more</a
+            >
+          </div>
+        </div>
+      </li> `;
+}
--- src/js/renderPopularNews.js
+++ src/js/renderPopularNews.js
@@ -1,23 +1,25 @@
-import { createCard } from "./cardMarkup"
-import { updateMarkup } from "./makkupUtils";
-import NewsApiServes from "./rest-api";
-const newsBoxEl = document.querySelector('.news-container') 
+import { createCard } from './cardMarkup';
+import { createCardPop } from './cardMarkup';
+import { updateMarkup } from './makkupUtils';
+import NewsApiServes from './rest-api';
+const newsBoxEl = document.querySelector('.news-container');
 const news = new NewsApiServes();
 
-export default async function renderPopularNews(){
-    console.log('happend')
-// try{
-    const response = await news.requestPopularNews()
-    const articles = response.data.results;
-        if (articles.length === 0) throw new Error("No data");
-    const markup = articles.reduce((markup, article)=> createCard(article) + markup, '')
-    updateMarkup(markup, newsBoxEl)
+export default async function renderPopularNews() {
+  console.log('happend');
+  // try{
+  const response = await news.requestPopularNews();
+  const articles = response.data.results;
+  if (articles.length === 0) throw new Error('No data');
 
-//   } catch{
-//     onError()
-//   } 
+  const markup = articles.reduce((markup, article) => createCardPop(article) + markup, '');
+  updateMarkup(markup, newsBoxEl);
+
+  //   } catch{
+  //     onError()
+  //   }
 }
 
-function onError(){
-    newsBoxEl.textContent = 'Not found any articles'
+function onError() {
+  newsBoxEl.textContent = 'Not found any articles';
 }
